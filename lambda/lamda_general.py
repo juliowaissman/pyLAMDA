e656de3ae3875566907ac60b1de19ab6a8a7e666
Julio Waissman Vilanova <juliowaissman@gmail.com>
2015-05-20 15:34:20 -0700

    Agregó la función de gad y unos TODOs

--- lambda/lamda_general.py
+++ lambda/lamda_general.py
@@ -32,3 +32,31 @@ def mad(rho, x):
         mads.append( np.power(rho[i, :], x) * np.power(1 - rho[i, :], 1 - x))
     return mads
 
+def gad(mads, operador):
+    """
+    Calcula el grado de adequación global para todas las clases
+
+    :param mads: lista de k matrics [M1, ..., Mk] de tamaño similar a x (datos) con los
+                 grados de adequación marginal de cada dato y cada descriptor, tal como
+                 se calculan con la función mads 
+    
+    :param operador: función tal que recibe un ndarray de dimensión n, m (con n objetos y 
+                     m descriptores) y regrese un ndarray vector columna tal que en la
+                     posición i, aplique el operador de agregación seleccionad a los
+                     datos del i´-ésimo renglon.
+    
+    :return: ndarray de dimensión n, k  con el grado de adecuación marginalde cada clase 
+             en cada dato, utilizando el operador de agregación seleccionado.
+    
+    """
+    # TODO:Hacer más clara la documentación de la función 
+    gads = np.zeros((mads[0].shape[0], len(mads)))
+    for (clase, mad) in enumerate(mads):
+        gads[:, clase] = operador(mad)
+    return gads
+        
+# TODO: Hacer el sistema en forma de clase con métodos (Lambda)
+# TODO: Hacer varias funciones de operadores de agregación para poder aplicar en LAMDA
+
+
+
